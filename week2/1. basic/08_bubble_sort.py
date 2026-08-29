e5926035e31c69982151ad9d5e0395cc0b127094
HYEONJI LEE <8976guswl@naver.com>
2026-08-29 22:57:59 +0900

    update : 버블 정렬 구현 오류 수정

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -23,12 +23,11 @@
 """
 
 def bubble_sort(arr):
-
     n = len(arr)
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+    for i in range(n - 1, -1, -1):
+        for j in range(0, i):
+            if arr[j] > arr[j + 1]: 
+              arr[j], arr[j + 1] = arr[j + 1], arr[j]
         
     return arr
 
@@ -43,13 +42,12 @@ def bubble_sort_optimized(arr):
         정렬된 배열
     """
     n = len(arr)
-    
-    for i in range(n - 1):
-        swapped = False  # 교환 발생 여부
-        for j in range(i, n):
-            if arr[i] > arr[j]:
-                swapped = True
-                arr[i], arr[j] = arr[j], arr[i]
+    for i in range(n - 1, -1, -1):
+        swapped = False
+        for j in range(0, i):
+            if arr[j] > arr[j + 1]: 
+              arr[j], arr[j + 1] = arr[j + 1], arr[j]
+              swapped = True
         if not swapped:
             break
         
